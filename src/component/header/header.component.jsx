9839f7fe61879e70c01c060ac081f2910415667a
Rom Fatal <39658012+RomFatal@users.noreply.github.com>
2023-02-10 10:19:31 +0200

    adding cart icon

--- src/component/header/header.component.jsx
+++ src/component/header/header.component.jsx
@@ -1,30 +1,36 @@
 import React from "react";
 import { Link } from 'react-router-dom'
 import { auth } from "../../firebase/firebase.utils";
-
+import CartIcon from '../cart-icon/cart-icon.componnet'
+import CartDropdown from "../cart-dropdown/cart-dropdown.componnet";
 import { ReactComponent as Logo } from '../../assets/084 crown.svg';
 
 import './header.style.scss'
 import { connect } from "react-redux";
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className="header">
         <Link to="/">
             <Logo className="logo" />
         </Link>
         <div className="options">
-            <Link className="options" to='/shop'>SHOP</Link>
-            <Link className="options" to='/contact'>CONTACT</Link>
+            <Link className="option" to='/shop'>SHOP</Link>
+            <Link className="option" to='/contact'>CONTACT</Link>
             {
-                currentUser ? <div className="options" onClick={() => auth.signOut()}>SIGN OUT</div>
-                    : <Link className="options" to='/sign-in'>SIGN IN</Link>
+                currentUser ? <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
+                    : <Link className="option" to='/sign-in'>SIGN IN</Link>
             }
+            <CartIcon />
         </div>
+        {
+            hidden ? null :
+                <CartDropdown />
+        }
     </div>
 )
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+    currentUser, hidden
 })
 
 export default connect(mapStateToProps)(Header);
