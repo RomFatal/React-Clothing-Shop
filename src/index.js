fc1a63b2d2239398f1b6e5b760145773b3b1afcf
Rom Fatal <39658012+RomFatal@users.noreply.github.com>
2023-02-13 11:58:49 +0200

    adding persist

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
 import './index.css';
 import App from './App';
-import store from './redux/store';
+import {store, persistor} from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <BrowserRouter>
       <React.StrictMode>
+        <PersistGate persistor={persistor}>
         <App />
+        </PersistGate>
       </React.StrictMode>
     </BrowserRouter>
   </Provider>
